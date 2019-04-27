8e3bb967740cc81ad39c96a78d3ce32e922201c9
mehyaa <mehyaa@gmail.com>
2019-04-27 11:31:29 +0300

    Rename

---
+++ Smarties.SocialTagMe.Web/Controllers/ImageController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Smarties.SocialTagMe.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImageController : ControllerBase
+    {
+        [HttpPost]
+        public async Task Post(IFormFile file)
+        {
+        }
+    }
+}
